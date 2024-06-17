cf29188e5bf2ba81fdb1b6d9060f096197ba6b22
steph-d989 <steb_d@hotmail.com>
2024-06-17 16:42:21 +0200

    Grafica y sonido

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const contenedorPreguntas = document.querySelector('.contenedorPreguntas');
 const fragment = document.createDocumentFragment();
+const rightAudio = new Audio('assets/RightAudio.ogg');
+const wrongAudio = new Audio('assets/WrongAudio.ogg');
+
+/* 
 
 const getQuestions = async () => {
     try {
@@ -20,7 +24,7 @@ const getQuestions = async () => {
 };
 
 const pintarQuestions = (arr) => {
-    arr.forEach((element, index)=> {
+    arr.forEach((element, index) => {
         const card = document.createElement('article');
         //esto
         card.classList.add('cardPreguntas')
@@ -28,20 +32,20 @@ const pintarQuestions = (arr) => {
         contenedorOptions.setAttribute('id', 'contenedorOptions')
         const question = document.createElement('h4');
         const category = document.createElement('h5');
-        const option1 = document.createElement('button'); 
+        const option1 = document.createElement('button');
         //
-        option1.classList.add('option1','respuestaBtn')
+        option1.classList.add('option1', 'respuestaBtn')
         const option2 = document.createElement('button');
         //
-        option2.classList.add('option2','respuestaBtn')
+        option2.classList.add('option2', 'respuestaBtn')
         const option3 = document.createElement('button');
         //
-        option3.classList.add('option3','respuestaBtn')
+        option3.classList.add('option3', 'respuestaBtn')
         const option4 = document.createElement('button');
         //
-        option4.classList.add('option4','respuestaBtn')
+        option4.classList.add('option4', 'respuestaBtn')
         const next = document.createElement('button');
-        next.textContent='Next' //
+        next.textContent = 'Next' //
         next.classList.add('nextBtn')// 
         //(option1, option2, option3, option4).classList.add('respuestaBtn');
         next.classList.add('nextBtn');
@@ -50,11 +54,12 @@ const pintarQuestions = (arr) => {
         const incorrectAnswersArr = element.incorrect_answers;
         shuffle(incorrectAnswersArr, element.correct_answer);
         console.log(element.incorrect_answers);
-        
-        // option1.textContent = element.incorrect_answers[0];
-        // option2.textContent = element.incorrect_answers[1];
-        // option3.textContent = element.incorrect_answers[2];
-        // option4.textContent = element.incorrect_answers[3];
+
+        option1.textContent = element.incorrect_answers[0];
+        option2.textContent = element.incorrect_answers[1];
+        option3.textContent = element.incorrect_answers[2];
+        option4.textContent = element.incorrect_answers[3];
+
         contenedorOptions.append(option1, option2, option3, option4);
         card.append(question, category, contenedorOptions, next);
         fragment.append(card);
@@ -67,4 +72,59 @@ const shuffle = (arr, correctAnswer) => {
     return arr.splice(random, 0, correctAnswer);
 };
 
+ */
+//Grafica de resultados
+
+
+const pintarGraficaResults = async () => {
+    const grafica = document.getElementById('graficaResultados').getContext('2d');
+
+    let results = JSON.parse(localStorage.getItem('resultados')) || [];
+    fechaJuego = [];
+    scoreJuego = [];
+
+    results.forEach((element)=>{
+        fechaJuego.push(element.fechaJuego)
+        scoreJuego.push(element.scoreJuego)
+    })
+    new Chart(grafica, {
+        type: 'line',
+        data: {
+            labels: fechaJuego,
+            datasets: [{
+                label: 'Your Stats',
+                data: scoreJuego,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    pintarGraficaResults();
+})  
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const volumen = document.querySelector('.volumen');
+    const musicaFondo = document.querySelector('.musicaFondo');
+    musicaFondo.play()
+    volumen.addEventListener('click', () => {
+        if (musicaFondo.paused) {
+            musicaFondo.play()
+        } else {
+            musicaFondo.pause();
+        }
+    });
+});
 getQuestions();
+pintarQuestions();
